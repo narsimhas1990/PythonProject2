bcb7f76454eed2ce9f72fccf36fe63ad266e21ca
narsimhas1990 <118528166+narsimhas1990@users.noreply.github.com>
2023-01-04 13:52:50 +0530

    asd

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 print("New Commit asacasddasdzxcsa5");
-items = {"Coffee": 2.2, "Tea": 1.5, "Chocolate": 2.5}
-for item in items.keys()
-     income = 0
-      qty = input(f"How many {item}s have you sold? ")
-     income = income + qty * items[item]
-print(f"\nThe income today was £{income:0.2f}")
+
+
 
